ad02758d1a02893ec0edece40440a67b02619e73
Abdullah Armiyao <53265844+AbdullahArmiyao@users.noreply.github.com>
2025-04-11 07:44:10 +0000

    Added puul-to-refresh feature, and wallpapers to change based on either day or night time. I also added other weather components, as well as icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  late final GifController _controller;  
+  late final GifController _controller;
   // So basically Map is like a tuple with the first value being a string  in
   // our project and the second being of any data type since it is dynamic
   // and is usually used for api calls
@@ -64,52 +64,246 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     }
   }
 
- 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather', textAlign: TextAlign.center,), 
-      // backgroundColor: Colors.transparent, elevation: 0,
-      ),
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Gif(
-            image: AssetImage("assets/gif.gif"),
-            controller: _controller,
-            // fps: 30
-            // duration: const Duration(seconds: 5)
-            autostart: Autostart.loop,
-            placeholder: (context) => const Text("Loading..."),
-            onFetchCompleted: () => {
-              if(mounted){
-                _controller.reset(),
-                _controller.forward(),
-              }
-            },
-            fit: BoxFit.cover,
-          ),
+    IconData getWeatherIcon(int condition) {
+      if (condition == 0 || condition == 1) {
+        return Icons.wb_sunny;
+      } else if (condition == 2) {
+        return Icons.wb_cloudy_outlined;
+      } else if (condition == 3) {
+        return Icons.wb_cloudy;
+      } else if (condition == 45 || condition == 48) {
+        return Icons.foggy;
+      } else if (condition == 51 || condition == 53 || condition == 55) {
+        return Icons.grain;
+      } else if (condition == 61 || condition == 63 || condition == 65) {
+        return Icons.umbrella;
+      } else if (condition == 66 || condition == 67) {
+        return Icons.ac_unit;
+      } else if (condition == 80 || condition == 81 || condition == 82) {
+        return Icons.grain;
+      } else if (condition == 95) {
+        return Icons.thunderstorm;
+      } else if (condition == 96 || condition == 99) {
+        return Icons.flash_on;
+      }
 
+      return Icons.help_outline;
+    }
+
+    String getDayTime(int dayTime) {
+      if (dayTime == 0) {
+        return "Nighttime";
+      } else if (dayTime == 1) {
+        return "Daytime";
+      }
+      return "Couldn't get day value";
+    }
+
+    String getWeatherCondition(int condition) {
+      if (condition == 0) {
+        return "Clear sky";
+      } else if (condition == 1) {
+        return "Mainly clear";
+      } else if (condition == 2) {
+        return "Partly cloudy";
+      } else if (condition == 3) {
+        return "Overcast";
+      } else if (condition == 45 || condition == 48) {
+        return "Foggy";
+      } else if (condition == 51 || condition == 53 || condition == 55) {
+        return "Drizzle";
+      } else if (condition == 61 || condition == 63 || condition == 65) {
+        return "Rain";
+      } else if (condition == 66 || condition == 67) {
+        return "Freezing rain";
+      } else if (condition == 71 || condition == 73 || condition == 75) {
+        return "Snowfall";
+      } else if (condition == 77) {
+        return "Snow grains";
+      } else if (condition == 80 || condition == 81 || condition == 82) {
+        return "Rain showers";
+      } else if (condition == 85 || condition == 86) {
+        return "Snow showers";
+      } else if (condition == 95) {
+        return "Thunderstorm";
+      } else if (condition == 96 || condition == 99) {
+        return "Thunderstorm with hail";
+      }
+      return "Couldn't identify weather condition";
+    }
+
+    String wallpaperPath() {
+      try {
+        if (weather == null) {
+          return "";
+        }
+        if (weather!['is_day'] == 0) {
+          return "assets/gif3.gif";
+        } else if (weather!['is_day'] == 1) {
+          return "assets/gif4.gif";
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.toString())));
+      }
+      return "";
+    }
+
+    Widget getCurrentWallpaper(String wallpaper) {
+      if (wallpaper == "assets/gif4.gif") {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              getWeatherIcon(weather!['weathercode']),
+              size: 48,
+              color: Colors.black,
+            ),
+            Text(locationName,
+                style: TextStyle(color: Colors.black, fontSize: 24)),
+            SizedBox(height: 10),
+            Text(getDayTime(weather!['is_day']),
+                style: TextStyle(color: Colors.black)),
+            Text('${weather!['temperature']}°C',
+                style: TextStyle(color: Colors.black, fontSize: 48)),
+            Text(getWeatherCondition(weather!['weathercode']),
+                style: TextStyle(color: Colors.black)),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Wind Speed: ${weather!['windspeed']} km/h',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Wind direction: ${weather!['winddirection']}',
+                style: TextStyle(color: Colors.black)),
+          ],
+        );
+      } else if (wallpaper == "assets/gif3.gif") {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              getWeatherIcon(weather!['temperature']),
+              size: 48,
+              color: Colors.white,
+            ),
+            Text(locationName,
+                style: TextStyle(color: Colors.white, fontSize: 24)),
+            SizedBox(height: 10),
+            Text(getDayTime(weather!['is_day']),
+                style: TextStyle(color: Colors.white)),
+            Text('${weather!['temperature']}°C',
+                style: TextStyle(color: Colors.white, fontSize: 48)),
+            Text(getWeatherCondition(weather!['weathercode']),
+                style: TextStyle(color: Colors.white)),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Wind Speed: ${weather!['windspeed']} km/h',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Wind direction: ${weather!['winddirection']}',
+                style: TextStyle(color: Colors.white)),
+          ],
+        );
+      }
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            getWeatherIcon(weather!['temperature']),
+            size: 48,
+            color: Colors.white,
           ),
-          Center(
-        // check if the weather data is null and if not, display the information
-        child: weather == null
-            ? CircularProgressIndicator(color: Colors.white,)
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+          Text(locationName,
+              style: TextStyle(color: Colors.white, fontSize: 24)),
+          SizedBox(height: 10),
+          Text(getDayTime(weather!['is_day']),
+              style: TextStyle(color: Colors.white)),
+          Text('${weather!['temperature']}°C',
+              style: TextStyle(color: Colors.white, fontSize: 48)),
+          Text(getWeatherCondition(weather!['weathercode']),
+              style: TextStyle(color: Colors.white)),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Wind Speed: ${weather!['windspeed']} km/h',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text('Wind direction: ${weather!['winddirection']}',
+              style: TextStyle(color: Colors.white)),
+        ],
+      );
+    }
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(
+      //     'Weather',
+      //     textAlign: TextAlign.center,
+      //   ),
+        // backgroundColor: Colors.transparent, elevation: 0,
+      // ),
+      body: RefreshIndicator(
+        onRefresh: loadWeather,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          child: SafeArea(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
                 children: [
-                  Text(locationName, style: TextStyle( 
-                    color: Colors.white, 
-                    fontSize: 24
-                    )
+                  Positioned.fill(
+                    child: Gif(
+                      image: AssetImage(wallpaperPath()),
+                      controller: _controller,
+                      // fps: 30
+                      // duration: const Duration(seconds: 5)
+                      autostart: Autostart.loop,
+                      placeholder: (context) => const Text("Loading..."),
+                      onFetchCompleted: () => {
+                        if (mounted)
+                          {
+                            _controller.reset(),
+                            _controller.forward(),
+                          }
+                      },
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  SizedBox(height: 10),
-                  Text('${weather!['temperature']}°C',
-                      style: TextStyle(color: Colors.white, fontSize: 48)),
-                  Text('Wind: ${weather!['windspeed']} km/h', style: TextStyle(color: Colors.white,),),
+                  Center(
+                      // check if the weather data is null and if not, display the information
+                      child: weather == null
+                          ? CircularProgressIndicator(
+                              color: Colors.black,
+                            )
+                          : getCurrentWallpaper(wallpaperPath())),
                 ],
-              ),),],
+              ),
+            ),
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 0,
@@ -125,8 +319,8 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         ],
         onTap: (index) {
           if (index == 0) {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => HomeScreen()));
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => HomeScreen()));
           } else if (index == 1) {
             AuthService().signOut();
             Navigator.pushReplacement(context,
